52c3462670e09518abc814c078260dada7bc1141
shubhamk-coder <shubhamkamble8983@gmail.com>
2022-09-04 00:32:20 +0530

    learning react

---
+++ one-helloworld/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.css";
+
+import Button from "./Button";
+
+const App = () => {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}> Hello World !! </h1>
+      <Button />
+      {/* <button className="button">App Store</button>
+            <button className="button">Play Store</button> */}
+    </div>
+  );
+};
+
+export default App;
---
+++ one-helloworld/src/Button.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const Button = () => {
+    <div>
+    <button className="button">App Store</button>
+    </div>
+}
+
+export default Button;
